8dd91bb0605f38d98c85ea0770e7394ceb3efcc2
Alessandro Spiridigliozzi <ale@alessandros-mbp.home>
2021-09-14 21:35:01 +0100

    added variable with boolean value to know in which array the values are pushed into

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const display = document.querySelector('#display');
 
 // get the value from each button, display it and add a click event to each one of them.
 
-let number1;
-let number2;
+let number1 = []; // Initialize the array
+let number2 = []; // Initialize the array
+// Keep a boolean value to know which number you are filling. You will push values into number array based on this value. It will be set to false once you press an operator.
+let isNumber1Filled = true; 
 let operator;
 
 const getValues = () => {
-    const numValue = document.querySelectorAll('.number');
+  const numValue = document.querySelectorAll('.number');
 
     numValue.forEach(number => {
         number.addEventListener('click', () => {
-            if(typeof number1 === 'undefined') {
-                number1 = number.value;
+            if (isNumber1Filled) { // Note the modified if/else conditions
+                number1.push(number.value)
                 display.innerHTML += ` ${number.value}  `;
                 console.log(number1);
-            } else if (typeof number1 !== 'undefined' && typeof number2 === 'undefined') {
-                number2 = number.value;
+            } else {
+                number2.push(number.value) // pushing value in number2 as well
                 display.innerHTML += ` ${number.value}  `;
                 console.log(number2)
             }
@@ -26,45 +28,48 @@ const getValues = () => {
 
     const getOperators = () => {
         const operatorValue = document.querySelectorAll('.operator');
-    
+
         operatorValue.forEach(operatorBtn => {
             operatorBtn.addEventListener('click', () => {
-                if(typeof operator === 'undefined') {
+                if (typeof operator === 'undefined') {
                     operator = operatorBtn.value;
                     display.innerHTML += `  <b>${operatorBtn.value}</b>  `;
+                    isNumber1Filled = false; // Making isFillingNumber1 false so as to enter in number2
                     console.log(operator);
                 }
             })
         })
     }
-    
-    getOperators();
 
-    const calculation = () => {
+  getOperators();
+
+  const calculation = () => {
         const equal = document.querySelector('.equal')
 
         equal.addEventListener('click', () => {
-            if(typeof number1 !== 'undefined' && typeof number2 !== 'undefined' && operator.includes('+')) {
-                console.log(+number1 + +number2)
-                let result = +number1 + +number2;
-                display.innerHTML += ` = ${result} `;
-                return result;
+            number1 = number1.join(""); // Joining the array to make a string.
+            number2 = number2.join("");
+            if (typeof number1 !== 'undefined' && typeof number2 !== 'undefined' && operator.includes('+')) {
+                    console.log(+number1 + +number2)
+                    let result = +number1 + +number2;
+                    display.innerHTML += ` = ${result} `;
+                    return result;
             } else if (typeof number1 !== 'undefined' && typeof number2 !== 'undefined' && operator.includes('-')) {
-                console.log(+number1 - +number2)
-                let result = +number1 - +number2;
-                display.innerHTML += ` = ${result} `;
+                    console.log(+number1 - +number2)
+                    let result = +number1 - +number2;
+                    display.innerHTML += ` = ${result} `;
             } else if (typeof number1 !== 'undefined' && typeof number2 !== 'undefined' && operator.includes('x')) {
-                console.log(+number1 * +number2)
-                let result = +number1 * +number2;
-                display.innerHTML += ` = ${result} `;
+                    console.log(+number1 * +number2)
+                    let result = +number1 * +number2;
+                    display.innerHTML += ` = ${result} `;
             } else if (typeof number1 !== 'undefined' && typeof number2 !== 'undefined' && operator.includes('/')) {
-                let result = +number1 / +number2;
-                display.innerHTML += ` = ${result} `;
-                console.log(+number1 / +number2)
+                    let result = +number1 / +number2;
+                    display.innerHTML += ` = ${result} `;
+                    console.log(+number1 / +number2)
             }
-        }) 
+        })
     }
-    calculation()
+  calculation()
 }
 
 getValues();
@@ -74,8 +79,8 @@ getValues();
 
 const clearAll = () => {
     const reset = document.getElementById('reset').addEventListener('click', () => {
-        number1 = undefined;
-        number2 = undefined;
+        number1 = [];
+        number2 = [];
         operator = undefined;
         display.innerHTML = "";
     })
